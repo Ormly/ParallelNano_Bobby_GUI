4a24c13c70debdcc5b0dfb2349f76d4938fc4943
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-08 22:42:29 +0100

    add button-container display internals

--- src/components/button-container.vue
+++ src/components/button-container.vue
@@ -1,10 +1,38 @@
 <template>
-
+  <div v-for="button in firstButtonRow" :key="button.label" class="px-1 py-1">
+    <base-button
+      label="button.label"
+      icon="button.icon"
+      size="button.size"
+      type="button.type">
+    </base-button>
+  </div>
+  <div v-for="button in secondButtonRow" :key="button.label" class="px-1 py-1">
+    <base-button
+        label="button.label"
+        icon="button.icon"
+        size="button.size"
+        type="button.type">
+    </base-button>
+  </div>
 </template>
 
 <script>
+import BaseButton from "@/components/base-button";
 export default {
-name: "button-container"
+name: "button-container",
+  components: {BaseButton},
+  props: {
+    buttonData: []
+  },
+  computed: {
+    firstButtonRow() {
+      return this.buttonData.slice(0,2)
+    },
+    secondButtonRow() {
+      return this.buttonData.slice(3,5)
+    }
+  }
 }
 </script>
 
