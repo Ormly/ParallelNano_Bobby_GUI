e694cc726b9ef5d5b4aa4a3cfa47acb3df2ade55
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-08 15:04:47 +0100

    clean up init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
@@ -9,6 +8,5 @@ Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
