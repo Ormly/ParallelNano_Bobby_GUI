1b552b981dc505c97ad60481994af7be6757e01d
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-08 15:30:19 +0100

    add further skeleton components

---
+++ src/components/nodes-management.vue
@@ -0,0 +1,15 @@
+<template>
+  <base-table></base-table>
+</template>
+
+<script>
+import BaseTable from "@/components/base-table";
+export default {
+name: "nodes-management",
+  components: {BaseTable}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/top-component.vue
@@ -0,0 +1,15 @@
+<template>
+  <nodes-management></nodes-management>
+</template>
+
+<script>
+import NodesManagement from "@/components/nodes-management";
+export default {
+name: "top-component",
+  components: {NodesManagement}
+}
+</script>
+
+<style scoped>
+
+</style>
