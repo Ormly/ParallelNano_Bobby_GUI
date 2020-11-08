fa95e58686aadb11b7b5d234595d03b3a48d1b26
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-08 21:39:38 +0100

    add base button file

---
+++ src/components/base-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-button
+      @click="clicked"
+      icon-left="icon"
+      size="size">
+    {{ label }}
+  </b-button>
+</template>
+
+<script>
+export default {
+name: "base-button",
+  props: {
+    label: String,
+    icon: String,
+    size: String
+  },
+  methods: {
+    clicked() {
+      this.$emit("clicked")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
