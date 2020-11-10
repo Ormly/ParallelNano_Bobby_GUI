341a90c6563ef33cbd74bb936ff3f32d4118dead
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-10 17:24:52 +0100

    implement first-draft data display

--- src/components/base-data-display.vue
+++ src/components/base-data-display.vue
@@ -1,10 +1,25 @@
 <template>
-
+    <div class="container">
+        <div class="box">
+            <b-icon
+                :icon="icon-name"
+                :size="icon-size">
+            </b-icon>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-name: "base-data-display"
+name: "base-data-display",
+    props: {
+        'icon-name': String,
+        'icon-size': String,
+        'current-value': Number
+    },
+    methods: {
+
+    }
 }
 </script>
 
