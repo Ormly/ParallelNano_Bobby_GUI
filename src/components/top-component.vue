f08a1acbee92f7eade9f5abc74e43a64acc0d155
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-10 14:26:28 +0100

    add users-management to top-container

--- src/components/top-component.vue
+++ src/components/top-component.vue
@@ -5,15 +5,19 @@
       <nodes-management class="column"/>
     </div>
     <div class="column">test</div>
-    <div class="column">test</div>
+    <div class="column">
+        <h1 class="is-size-1">Nodes</h1>
+        <users-management class="column"/>
+    </div>
   </div>
 </template>
 
 <script>
 import NodesManagement from "@/components/nodes-management";
+import UsersManagement from "@/components/user-management";
 export default {
 name: "top-component",
-  components: {NodesManagement}
+  components: {NodesManagement, UsersManagement}
 }
 </script>
 
