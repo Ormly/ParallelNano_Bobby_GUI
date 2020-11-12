efb64d4cf5ce9aa9fa783d574cc1c94220e2b959
Ormly <9220837+Ormly@users.noreply.github.com>
2020-11-12 17:29:24 +0100

    add yes-no-button-container skeleton

--- src/components/nodes-removenode-message.vue
+++ src/components/nodes-removenode-message.vue
@@ -1,10 +1,24 @@
 <template>
-
+    <b-message title="Remove Node" type="is-warning" size="is-large">
+        <h2 class="is-size-1">Are you sure you want to remove Node {{nodeToRemove}}</h2>
+        <br/>
+        <b-field>
+            <b-input size="is-medium" placeholder="Node Name" v-model="inputData"></b-input>
+            <p class="control">
+                <b-button size="is-medium" class="button" @click="submitData" is-primary>
+                    Submit
+                </b-button>
+            </p>
+        </b-field>
+    </b-message>
 </template>
 
 <script>
 export default {
-name: "nodes-removenode-message"
+name: "nodes-removenode-message",
+    props: {
+        nodeToRemove: String
+    }
 }
 </script>
 
